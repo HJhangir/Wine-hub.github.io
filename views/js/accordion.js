32b25fd0ab24acb1f501924fc8869ab29405110b
Cahangir Huseynov <cahangir@MacBook-Pro-Cahangir.local>
2023-09-18 01:56:06 +0400

    first commit

---
+++ views/js/accordion.js
@@ -0,0 +1,10 @@
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+    const question = item.querySelector('.question');
+    const answer = item.querySelector('.answer');
+
+    question.addEventListener('click', () => {
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
---
+++ views/js/active.js
@@ -0,0 +1,16 @@
+const navbar = document.getElementsByClassName("fa-bars")[0];
+const overlaymenu = document.querySelector(".overlay");
+const closex = document.querySelector(".fa-xmark");
+
+navbar.addEventListener("click", openOverlay);
+closex.addEventListener("click", closeeOverlay);
+
+function openOverlay() {
+	overlaymenu.classList.add("active");
+
+}
+
+function closeeOverlay() {
+	overlaymenu.classList.remove("active");
+
+}
